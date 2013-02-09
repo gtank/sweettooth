dd2449e876ca08b8216b50f6a148d53f0ed68058
George Tankersley <george.tankersley@gmail.com>
2013-02-09 13:47:22 -0500

    fix tabs

--- cc/gtank/bt/Bluetooth.java
+++ cc/gtank/bt/Bluetooth.java
@@ -1,7 +1,7 @@
 package cc.gtank.bt;
 
 public interface Bluetooth {
-	public boolean initializeHeadset();
-	public boolean startVoiceRecognition();
-	public boolean stopVoiceRecognition();
+    public boolean initializeHeadset();
+    public boolean startVoiceRecognition();
+    public boolean stopVoiceRecognition();
 }
--- cc/gtank/bt/Gingertooth.java
+++ cc/gtank/bt/Gingertooth.java
@@ -14,23 +14,23 @@ import android.util.Log;
 
 @TargetApi(Build.VERSION_CODES.GINGERBREAD)
 public class Gingertooth implements Bluetooth {
-	
-	private Context context;
-	private Object btHeadset;
-	private boolean connected;
-	
-	public Gingertooth(Context parent) {
-		context = parent;
-		connected = false;
-	}
+    
+    private Context context;
+    private Object btHeadset;
+    private boolean connected;
+    
+    public Gingertooth(Context parent) {
+        context = parent;
+        connected = false;
+    }
 
-	@Override
-	public boolean initializeHeadset() {
-		if(Build.VERSION.SDK_INT == 10) {
-			try {
-				Class<?> baseHeadset = Class.forName("android.bluetooth.BluetoothHeadset");
-				Class<?> serviceListener = baseHeadset.getDeclaredClasses()[0]; //android.bluetooth.BluetoothHeadset$ServiceListener
-				Object listenerProxy = java.lang.reflect.Proxy.newProxyInstance(serviceListener.getClassLoader(), 
+    @Override
+    public boolean initializeHeadset() {
+        if(Build.VERSION.SDK_INT == 10) {
+            try {
+                Class<?> baseHeadset = Class.forName("android.bluetooth.BluetoothHeadset");
+                Class<?> serviceListener = baseHeadset.getDeclaredClasses()[0]; //android.bluetooth.BluetoothHeadset$ServiceListener
+                Object listenerProxy = java.lang.reflect.Proxy.newProxyInstance(serviceListener.getClassLoader(), 
                         new Class[] { serviceListener },
                         new InvocationHandler() {
                             @Override
@@ -45,48 +45,48 @@ public class Gingertooth implements Bluetooth {
                                 return null;
                             }
                     
-					});
-				Constructor<?> constructor = baseHeadset.getConstructor(Context.class, serviceListener);
-				btHeadset = constructor.newInstance(context, listenerProxy);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return true;
-		} else {
-			return false;
-		}
-	}
+                    });
+                Constructor<?> constructor = baseHeadset.getConstructor(Context.class, serviceListener);
+                btHeadset = constructor.newInstance(context, listenerProxy);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	@Override
-	public boolean startVoiceRecognition() {
-		if(connected) {
-			try {
-				Method startVoice = btHeadset.getClass().getMethod("startVoiceRecognition");
-				boolean started = (Boolean) startVoice.invoke(btHeadset, (Object[])null);
-				return started;
-			} catch (Exception e) {
-				e.printStackTrace();
-				return false;
-			}
-		} else {
-			return false;
-		}
-	}
+    @Override
+    public boolean startVoiceRecognition() {
+        if(connected) {
+            try {
+                Method startVoice = btHeadset.getClass().getMethod("startVoiceRecognition");
+                boolean started = (Boolean) startVoice.invoke(btHeadset, (Object[])null);
+                return started;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 
-	@Override
-	public boolean stopVoiceRecognition() {
-		if(connected) {
-			try {
-				Method stopVoice = btHeadset.getClass().getMethod("stopVoiceRecognition");
-				boolean stopped = (Boolean) stopVoice.invoke(btHeadset, (Object[])null);
-				return stopped;
-			} catch (Exception e) {
-				e.printStackTrace();
-				return false;
-			}
-		} else {
-			return false;
-		}
-	}
+    @Override
+    public boolean stopVoiceRecognition() {
+        if(connected) {
+            try {
+                Method stopVoice = btHeadset.getClass().getMethod("stopVoiceRecognition");
+                boolean stopped = (Boolean) stopVoice.invoke(btHeadset, (Object[])null);
+                return stopped;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 
 }
